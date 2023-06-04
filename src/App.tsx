218ec5d8cc1a8975534b843a54c048c18a1d8023
ymakwan1 <ymakwan1@binghamton.edu>
2023-06-04 16:15:20 -0400

    .

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My GitHub Pages</h1>
+        <p>Replace this content with your own!</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
