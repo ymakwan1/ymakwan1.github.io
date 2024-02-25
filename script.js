b5552112b435eb8f700b08718e2286abe5672e2c
ymakwan1 <ymakwan1@binghamton.edu>
2024-02-25 12:29:34 -0500

    minor fixes

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
