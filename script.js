c6e014565d002e5d80a426049d8e75dd75ec065b
ymakwan1 <ymakwan1@binghamton.edu>
2024-02-25 14:44:58 -0500

    added about me

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// script.js
 // Smooth scrolling
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
@@ -8,4 +7,42 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
             behavior: 'smooth'
         });
     });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const headingElement = document.getElementById('welcome-heading');
+    const heading = "Delighted to see you!";
+    let headingIndex = 0;
+
+    // Typewriter effect script for p tag
+    const messageElement = document.getElementById('welcome-message');
+    const message = "yash@127.0.0.1:~/$: cat welcome.txt \n yash@127.0.0.1:~/$: I am Yash, Nice to meet you!";
+    let messageIndex = 0;
+
+    function typeMessage() {
+        if (messageIndex < message.length) {
+            if (message.charAt(messageIndex) === '\n') {
+                messageElement.innerHTML += '<br>'; // Add line break
+            } else {
+                messageElement.innerHTML += message.charAt(messageIndex);
+            }
+            messageIndex++;
+            setTimeout(typeMessage, 100); // Adjust typing speed here (in milliseconds)
+        } else {
+            // Fade out the elements after the typewriter effect is complete
+            setTimeout(function() {
+                headingElement.style.transition = "opacity 1s";
+                messageElement.style.transition = "opacity 1s";
+                headingElement.style.opacity = "0";
+                messageElement.style.opacity = "0";
+                // Navigate to the About Me page after fadeout
+                setTimeout(function() {
+                    window.location.href = "about.html";
+                }, 1000); // Adjust timing as needed
+            }, 2000); // Adjust timing as needed
+        }
+    }
+
+    // Start the typewriter effect for the message
+    typeMessage();
 });
