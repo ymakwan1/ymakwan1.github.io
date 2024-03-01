4994b44eacf2ebb1829601794ee82f6d87a68b3b
ymakwan1 <ymakwan1@binghamton.edu>
2024-03-01 16:49:57 -0500

    rewind with tailwind CSS

--- projects/scripts.js
+++ projects/scripts.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.body.classList.add("loaded");
 });
+
+document.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const href = this.getAttribute('href');
+        document.body.style.opacity = 0;
+        setTimeout(function() {
+            window.location.href = href;
+        }, 500); // Adjust the delay time as needed
+    });
+});
--- script.js
+++ script.js
@@ -1,54 +1,25 @@
-// Smooth scrolling
+document.addEventListener("DOMContentLoaded", function() {
+    // Fade out the text after animation
+    setTimeout(function() {
+        const elements = document.querySelectorAll('.fade-out');
+        elements.forEach(function(element) {
+            element.style.opacity = '0';
+        });
+    }, 2000); 
+
+    // Navigate to the About page after fading out
+    setTimeout(function() {
+        window.location.href = "/about/";
+    }, 3500); 
+});
+
+// Smooth scrolling for anchor links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
+    anchor.addEventListener('click', function (e) {
         e.preventDefault();
 
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
     });
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-    const headingElement = document.getElementById('welcome-heading');
-    const heading = "Delighted to see you!";
-    let headingIndex = 0;
-
-    // Typewriter effect script for p tag
-    const messageElement = document.getElementById('welcome-message');
-    //const message = "yash@127.0.0.1:~/$: cat welcome.txt \n yash@127.0.0.1:~/$: I am Yash, Nice to meet you!";
-    const message = "Delighted to see you. \n I am Yash, Nice to meet you!"
-    let messageIndex = 0;
-
-    function typeMessage() {
-        if (messageIndex < message.length) {
-            if (message.charAt(messageIndex) === '\n') {
-                messageElement.innerHTML += '<br>'; 
-            } else {
-                messageElement.innerHTML += message.charAt(messageIndex);
-            }
-            messageIndex++;
-            setTimeout(typeMessage, 100); 
-        } else {
-            // Fade out the elements after the typewriter effect is complete
-            setTimeout(function() {
-                headingElement.style.transition = "opacity 1s";
-                messageElement.style.transition = "opacity 1s";
-                headingElement.style.opacity = "0";
-                messageElement.style.opacity = "0";
-                // Navigate to the About Me page after fadeout
-                setTimeout(function() {
-                    window.location.href = "about/";
-                }, 1000); 
-            }, 2000); 
-        }
-    }
-
-    // Start the typewriter effect for the message
-    typeMessage();
-});
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    document.body.classList.add("loaded");
 });
